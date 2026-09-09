3dc06104ffc29d765ead337f322a08fb36c7e5ad
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-09 10:56:05 -0700

    add DivingState.cs

---
+++ Nystrom's Heroine/Assets/Scripts/DivingState.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Chapter.State
+{
+    public class DivingState : IHeroineState
+    {
+        private readonly Heroine _heroinie;
+        
+        public StandingState(Heroine heroine)
+        {
+
+        }
+        public void Update()
+        {
+
+        }
+    }
+}
+
+
