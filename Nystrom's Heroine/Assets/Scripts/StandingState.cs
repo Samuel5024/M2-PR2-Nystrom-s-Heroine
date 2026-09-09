b0436dd7958382aa1eaa835ab354ea5857dd1369
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2026-09-09 10:14:54 -0700

    Add StandingState.cs

---
+++ Nystrom's Heroine/Assets/Scripts/StandingState.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class StandingState : IHeroineState
+{
+    private readonly Heroine _heroine;
+
+    public StandingState(Heroine heroine)
+    {
+        _heroine = heroine;
+    }
+
+    public void Enter()
+    {
+        Debug.Log("Enter Standing");
+    }
+
+    public void HandleInput()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            _heroine.SetHeroineState(new JumpingState(_heroine));
+        }
+
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            _heroine.SetHeroineState(new DuckingState(_heroine));
+        }
+    }
+   
+    // Update is called once per frame
+    public void Update()
+    {
+        
+    }
+}
